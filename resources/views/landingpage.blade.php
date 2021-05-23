2984d976e2004de0c88447b00cf1581bdff5dea1
Wachira Nduati <wachiranduati@gmail.com>
2021-05-24 00:54:11 +0300

    setup african asili logo and button

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('content')
+<!-- <img src="/images/sergey-pesterev-unsplash.jpg" alt="image"> -->
+<div class="container-fluid" style="background-image:url('/images/sergey-pesterev-unsplash.jpg'); background-size: 100%;height: 100vh;">
+    <div class="row">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
+            <div class="row gy-4" style="margin-top: 20%;">
+                <div class="col-lg-12">
+                    <img src="/images/africanasili_whitelogo.png" alt="" class="img-fluid">
+                </div>
+                <div class="col-lg mx-auto">
+                    <a class="btn btn-primary mx-auto" style="display:block;background-color: #264653;border-radius:0;color:white;margin: 0 auto;font-family:Poppins;font-weight:bold;border:none;width: 40%;" href="www.google.com">GET STARTED</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3"></div>
+    </div>
+</div>
+@endsection
