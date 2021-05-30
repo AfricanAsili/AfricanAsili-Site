3da54332e9bdd81e4c78519775440140344ee6c0
Wachira Nduati <wachiranduati@gmail.com>
2021-05-31 00:01:33 +0300

    add icon title to sites, and refactor navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,7 @@
     <title>Landing Page</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="images/as_title.png">
 </head>
 <body>
     @yield('content')
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,15 @@
+<div class="container-fluid" style="background-color: #264653;">
+    <div class="row">
+        <div class="col-lg-2">
+            <a class="navbar-brand" href="#"><img src="/images/africanasililogo.png" alt="" class="img-fluid" style="height: 40px;margin-top: 1%;"></a>
+        </div>
+        <div class="col-lg-4">
+            <input type="search" placeholder="Search i.e Country, date, key phrase" name="" id="" style="border: none; border-radius: 4px;padding: 5px 7px;height: 85%;margin-top: 1%;width: 100%;">
+        </div>
+        <div class="col-lg-2" style="margin: auto 0;color: white;"><a class="nav-link" href="#" style="color: white;"> <h5>Explore</h5> </a></div>
+        <div class="col-lg-2" style="margin: auto 0;color: white;"><a class="nav-link" href="#" style="color: white;"> <h5>Notifications</h5> </a></div>
+        <div class="col-lg-2">
+                <a href="/login"><img src="/images/sergey-pesterev-unsplash.jpg" alt="" class="img-fluid" style="border-radius: 50%;height: 40px; width: 40px;margin: 1%;border: 1px solid white;float: right;margin-top: 2%;"></a>
+        </div>
+    </div>
+</div>
