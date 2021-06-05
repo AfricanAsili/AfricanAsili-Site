93d6d91bac4fc433434a24078a65bf203c590e5d
Wachira Nduati <wachiranduati@gmail.com>
2021-06-05 21:57:00 +0300

    setup about us page

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -37,11 +37,203 @@
                     <div class="col-lg-10">
                         <div class="row">
                             <div class="col-lg">
-                                <h2 style="text-align: center;font-weight: bold;font-size: 50px;">ABOUT US</h2>
+                                <div class="row">
+                                    <div class="col-lg">
+                                        <h2 style="text-align: center;font-weight: bold;font-size: 50px;">ABOUT US</h2>
+                                    </div>
+                                </div>
+                                <div class="row" style="padding: 10px;margin-bottom:10px;">
+                                    <div class="col-lg" style="font-family: poppins;background-color: white;border: 1px solid #707070;padding-top: 15px;padding-bottom: 15px;">
+                                        <h3 style="font-weight: bold;">The story of how it all began</h3>
+                                        <small>Kind Disclaimer. I'm not a writer but I'll try my best to create an idea of the reason I started African Asili.</small>
+                                        <small>I have for the longest time, ever since getting an internet connection been a lover of documentaries. I am a science nerd and never
+                                            did history in highschool but for some reason having consumed soo much western, European and Asian documentaries felt we had a
+                                            great deficit of African historic documentaries. Worse yet told by the people themselves. It's been said before that Africa's greatest 
+                                            downfall was never documenting her history in written records.<br>
+                                            <br>
+                                            It was once passed down from generations but to date greed ignorance and capitalism has broken that historic link we once had.
+                                            It is now more clearer than ever that we are on the verge of losing our history for ever. We live in the digital age but having to depend
+                                            on "others" to research and tell us about our history is..there's something wrong.<br>
+                                            someone once said "the one who wins the war writes history books"<br>
+                                            Why not capture that part of history that is hidden all over. Not only the bad but also the good. We have a lot of private collectors all
+                                            over the world, including the European Museums, now this is not a plea to get the privately collected items back to Africa, most if not
+                                            all African countries ~governments~ don't seem to have a need for the museums. We the people do, donate the items collected
+                                            privately as digital records, anonymously if you need to and help us record our history collaboratively.</small>
+                                    </div>
+                                </div>
+                            
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row" style="background-image: url('/images/africa_map.png');background-size: cover;background-attachment:fixed;">
+                    <div class="col-lg">
+                        <div class="row" style="margin-top: 3%;margin-bottom: 3%;">
+                            <div class="col-lg-2"></div>
+                            <div class="col-lg-8" style="background-color: white;font-family: poppins;padding: 15px;">
+                                <h3 style="font-weight: bold;">Our Mission</h3>
+                                <small>
+                                    To collaboratively reconstruct the African Narrative.<br><br>
+
+How will we go about this you ask?<br>
+Everyone has a part to play, your grandparents being the very first source of this historic link. Our intention is to record this wealth
+of history they have. Will it be about traditions, colonization, life in the earlier day, historical event records or plants? We want it all.<br><br>
+                                </small>
+                                <small>
+                                    Institutions can contribute records from their archives, photos, journals and much more.
+This will also serve as a drive for future generations. It's said that greed and corruption has been the biggest hurdle towards the 
+growth of Africa as a continent, today will mark a day in the future as a historical event We will forever record it. Let your future 
+generations learn about the greedy few who impeded the growth of the nation.
+                                </small>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-lg" style="background-color: white;">
+                        <div class="row">
+                            <div class="col-lg" style="padding: 15px;">
+                                <h3>The Team</h3>
+                                <a class="text-muted" href="https://www.linkedin.com/in/wachira-nduati/" style="text-decoration: none;">Wachira N Nduati - Creator of AfricanAsili</a>
+                                <div class="row">
+                                    <div class="col-lg-2">
+                                        <ul class="list-group" style="list-style: none;">
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <ul class="list-group" style="list-style: none;">
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <ul class="list-group" style="list-style: none;">
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <ul class="list-group" style="list-style: none;">
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <ul class="list-group" style="list-style: none;">
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <ul class="list-group" style="list-style: none;">
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                            <li><small>Your name could go here</small></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <p style="text-align: center;margin-top: 15px;color: black;font-family:poppins;font-weight: bold;font-size: 12px;">Contribute to have your name featured in here...</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="row">
+                            <div class="col-lg" style="background-color: #F1F1F1;padding: 10px 10px 40px 10px;font-family:poppins;">
+                                <h3 style="font-weight:bold;padding-top: 20px;">CONTRIBUTING</h3>
+                                <small style="">
+                                    This is probably the easiest part. The first and most important way you can help us is by documenting history. Have your parents,
+grandparents and friends share the part of history they can get to. <br>
+Visit your local museums and historical sites, document them and share them with us and the rest of the world. <br><br>
+
+More to this you can help us grow the platform. Our codebase is open source and as such you can help find bugs, build features 
+and the like. You don't have to be a techie to help us build this, you can give us feature requests. You can become a writer, an editor, 
+a graphic designer and we've not scratched the surface. You have a skill or ability that can help us and we would really appreciate
+getting that help. AfricanAsili is and will forever be community owned.
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg" style="height: 200px;padding: 10px 10px 40px 10px;font-family:poppins;">
+                        <div>
+                            <h3 style="margin-left: 20%;font-weight: bold;">Want to Help?</h3>
+                        <small>Techie? Check out any of our repositories? Make a PullRequest</small>
+                        <ul class="list-group" style="margin-left: 4%;list-style:none;">
+                            <li><small>Backend - Laravel</small></li>
+                            <li><small>FrontEnd - HTML, CSS3, VUE</small></li>
+                            <li><small>Android - Kotlin</small></li>
+                        </ul>
+                        <small>Not a techie? There's something you can definitely help us with...here's an example.</small>
+                        <div style="margin-left: 4%;">
+                            <small>we need graphic designers to help us make our UIs look better</small><br>
+                            <small>can you help us make our content more friendly to our users who might have a disability? We need you</small><br>
+                            <small>We're sure you have a skill that can help us out.</small>
+                        </div>
+                        <p style="text-align: center;margin-bottom: 20px;"><small>Got Questions? <span><strong>support@africanasili.org</strong></span></small></p>
+                    
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
